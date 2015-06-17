00f8904d60d3ac968408ef13e0b1b45d9a2b4cce
Sasha Shanina <alekshanina@gmail.com>
2015-06-17 16:20:51 -0400

    cleaned up the repo & deleted ignored files from cache

--- run2.sh
+++
@@ -1,7 +0,0 @@
-#rm -r /usr/local/hadoop-2.6.0/hbrj/output2
-#/usr/local/hadoop-2.6.0/bin/hadoop jar ~/mapreduce/build/Hadoop.jar test.RPhase2 -m 1 -r 6 -k 2 \
-#/usr/local/hadoop-2.6.0/hbrj/output /usr/local/hadoop-2.6.0/hbrj/output2
-
-/usr/local/hadoop-2.6.0/bin/hdfs dfs -rm -r /user/sasha/hbrj/output2
-/usr/local/hadoop-2.6.0/bin/hadoop jar ~/mapreduce/build/Hadoop.jar test.RPhase2 -m 1 -r 6 -k 2 \
-/user/sasha/hbrj/output /user/sasha/hbrj/output2
--- viewwordcount.sh
+++
@@ -1,2 +0,0 @@
-/usr/local/hadoop/bin/hdfs dfs -get /user/sasha/wordcount mrknnj-release/hbrj/output_wordcount
-sublime mrknnj-release/hbrj/output_wordcount
